975c4874e8bc0f143169cd5556cdec856614548c
Guilherme Teodoro <guilhermedoamaralteodoro@gmail.com>
2026-05-30 01:37:23 -0300

    Register Toast stimulus controllers, keep gem at 1.2.0 (components already generated)

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,8 +6,12 @@ import ImportFormController from "./import_form_controller"
 // RubyUI controllers
 import DropdownMenuController from "./ruby_ui/dropdown_menu_controller"
 import FormFieldController from "./ruby_ui/form_field_controller"
+import ToastController from "./ruby_ui/toast_controller"
+import ToasterController from "./ruby_ui/toaster_controller"
 
 application.register("clipboard", ClipboardController)
 application.register("import-form", ImportFormController)
 application.register("ruby-ui--dropdown-menu", DropdownMenuController)
 application.register("ruby-ui--form-field", FormFieldController)
+application.register("ruby-ui--toast", ToastController)
+application.register("ruby-ui--toaster", ToasterController)
