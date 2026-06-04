26933b35b82c2b590c3817afc2b2433674c161e5
Guilherme Teodoro <guilhermedoamaralteodoro@gmail.com>
2026-06-04 14:57:35 -0300

    fix(album): register album-card stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AlbumCardController from "./album_card_controller"
+application.register("album-card", AlbumCardController)
+
 import ClipboardController from "./clipboard_controller"
 application.register("clipboard", ClipboardController)
 
